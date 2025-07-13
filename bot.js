97b46841058dd4c02f13086cfb4a133a3cbc3f93
Solar <abe@solarcosmic.net>
2025-07-13 21:52:45 +1200

    Add Dynamic Content Type Function (v1.0.0)

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@ const { Client, Events, GatewayIntentBits, ContextMenuCommandBuilder, Applicatio
 const wait = require("node:timers/promises").setTimeout;
 const sharp = require("sharp");
 const axios = require("axios");
+const { StringSelectMenuBuilder } = require("@discordjs/builders");
+const { StringSelectMenuOptionBuilder } = require("@discordjs/builders");
 const dotenv = require("dotenv").config();
 const token = process.env.CLIENT_TOKEN;
 const rest = new REST({version: "10"}).setToken(token);
@@ -24,7 +26,7 @@ client.on("messageCreate", message => {
     console.log(message.content);
 })
 
-const pngButton = new ButtonBuilder()
+/* const pngButton = new ButtonBuilder()
     .setCustomId("png_select")
     .setLabel(".png")
     .setStyle(ButtonStyle.Primary);
@@ -35,70 +37,81 @@ const jpgButton = new ButtonBuilder()
     .setStyle(ButtonStyle.Secondary);
 
 const row = new ActionRowBuilder()
-    .addComponents(pngButton, jpgButton);
+    .addComponents(pngButton, jpgButton); */
+
+const format_sel = new StringSelectMenuBuilder()
+    .setCustomId("format_sel")
+    .setPlaceholder("Choose a format...")
+    .addOptions(
+        new StringSelectMenuOptionBuilder()
+            .setLabel("PNG (.png)")
+            .setDescription("Portable Network Graphics")
+            .setValue("image/png"),
+        new StringSelectMenuOptionBuilder()
+            .setLabel("JPG/JPEG (.jpg)")
+            .setDescription("Joint Photographic Experts Group Image")
+            .setValue("image/jpeg"),
+        new StringSelectMenuOptionBuilder()
+            .setLabel("WebP (.webp)")
+            .setDescription("Web Picture Format")
+            .setValue("image/webp")
+    )
+
+const row = new ActionRowBuilder()
+    .addComponents(format_sel);
 
 var toConvert = null;
+const convertMap = new Map();
 client.on(Events.InteractionCreate, async interaction => {
     if (!interaction.isMessageContextMenuCommand()) return;
-    const message = interaction.targetMessage.attachments.first();
-    //console.log(message);
-    if (!message) {
+    const attach = interaction.targetMessage.attachments.first();
+    convertMap.set(interaction.user.id, attach);
+    // continue
+    if (!attach) {
         await interaction.reply({content: `Image not found! Are you sure this is attached correctly?`, flags: MessageFlags.Ephemeral});
         return;
     }
-    if (message.contentType != "image/jpeg") {
+    if (!attach.contentType.includes("image")) {
         await interaction.reply({content: `The attachment provided seems to not be a supported image! Only PNG and JPEG file types are supported.`, flags: MessageFlags.Ephemeral});
         return;
     }
-    await interaction.reply({content: `What would you like to convert this image to (${message.name})?`, components: [row], flags: MessageFlags.Ephemeral});
-    toConvert = message;
+    await interaction.reply({content: `What would you like to convert this image to (${attach.name})?`, components: [row], flags: MessageFlags.Ephemeral});
+    toConvert = attach;
 })
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isButton()) return;
-    await interaction.update({content: "Converting your image to your selected format. This may take a while.", components: []});
-    //await wait(5_000);
-    if (toConvert) {
-        const img = await convertImage(toConvert.url, "image/png");
-        const attach = new AttachmentBuilder(img, {name: "converted.png"})
-        console.log(attach);
-        // https://embed.dan.onl/
-        /*const embed = new EmbedBuilder()
-        .setTitle(toConvert.name)
-        .setImage("Image")
-        .setColor("#00b0f4")
-        .setFooter({
-            text: "ConvertThat",
-        })
-        .setTimestamp();*/
 
-        //await message.reply({ embeds: [embed] });
-        const sent = await interaction.followUp({
-            embeds: [
-                new EmbedBuilder()
-                    .setTitle(attach.name)
-                    .addFields(
-                        {
-                            name: "Conversion",
-                            value: "JPG -> PNG",
-                            inline: true
-                        }
-                    )
-                    .setImage("attachment://converted.png")
-                    .setColor("#00b0f4")
-                    .setFooter({text: "ConvertThat"})
-                    .setTimestamp()
-            ],
-            files: [attach],
-            ephemeral: true
-        });
-        //await interaction.followUp({content: "Here is your converted image.", files: [{attachment: img, name: "converted.png"}], flags: MessageFlags.Ephemeral});
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isStringSelectMenu()) return;
+    const item = convertMap.get(interaction.user.id);
+    if (!item) {
+        interaction.update({content: "Session expired! Please retry.", components: []});
+        return;
     }
-    /*
-    if (interaction.customId == "png_select") {
-        await interaction.update({content: "Converting your image to your selected format. This may take a while.", components: []});
-    } else if (interaction.customId == "jpg_select") {
-        await interaction.update({content: "You chose JPG!", components: []});
-    } */
+    await interaction.update({content: "Converting your image to your selected format. This may take a while.", components: []});
+    const format = interaction.values[0];
+    const img = await convertImage(item.url, format);
+    const fileName = item.name.substring(0, item.name.lastIndexOf("."));
+    const combined = fileName + "." + format.slice(6);
+    console.log(combined);
+    const attach = new AttachmentBuilder(img, {name: combined});
+    const sent = await interaction.followUp({
+        embeds: [
+            new EmbedBuilder()
+                .setTitle(attach.name)
+                .addFields(
+                    {
+                        name: "Conversion",
+                        value: `${item.contentType.slice(6).toUpperCase()} -> ${format.slice(6).toUpperCase()}`,
+                        inline: true
+                    }
+                )
+                .setImage("attachment://" + combined)
+                .setColor("#00b0f4")
+                .setFooter({text: "ConvertThat"})
+                .setTimestamp()
+        ],
+        files: [attach],
+        ephemeral: true
+    });
 })
 
 async function convertImage(url, format) {
@@ -107,10 +120,14 @@ async function convertImage(url, format) {
     try {
         resp = await axios.get(url, {responseType: "arraybuffer"});
         if (resp) buffer = Buffer.from(resp.data);
-        if (format == "image/png") {
-            const sharpImg = await sharp(buffer).png().toBuffer();
-            return sharpImg;
-        }
+        return await sharp(buffer).toFormat(format.slice(6)).toBuffer();
+        /*if (format == "image/png") {
+            return await sharp(buffer).png().toBuffer();
+        } else if (format == "image/jpeg") {
+            return await sharp(buffer).jpeg().toBuffer();
+        } else if (format == "image/webp") {
+            return await sharp(buffer).webp().toBuffer();
+        }*/
     } catch (e) {
         console.log("Conversion error: " + e);
     }
